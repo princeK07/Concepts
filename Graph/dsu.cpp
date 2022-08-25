ceded321e25572cd9f92ca4e0631f1f476801cf3
l3vel <109853601+princeK07@users.noreply.github.com>
2022-08-25 12:24:04 +0530

    Update dsu.cpp

--- Graph/dsu.cpp
+++ Graph/dsu.cpp
@@ -1,1 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+#define nl "\n"
+
+const int N = 1e5+10;
+int parent[N], size[N];
+
+void make(int v){
+  parent[v]=v;
+  size[v]=1;
+}
+
+int find(int v){
+  if(parent[v]==v) return v;
+  parent[v]=find(parent[v]);
+
+  return parent[v];
+}
+
+void Union(int a, int b){
+  a = find(a);
+  b = find(b);
+
+  if(size[a] < size[b]) swap(a, b);
+
+  parent[b]=a;
+  size[a]+=size[b];
+
+}
+
+void solve(){
+  
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(false);
+  cout.tie(NULL);
+
+  int t=1; 
+  // cin>>t;
+  while(t--){
+    solve();
+    cout<<nl;
+  }
+
+  return 0;
+}
