1f962dff48d26ab0dc4c7d6e6aaba0d0ff8223d3
l3vel <109853601+princeK07@users.noreply.github.com>
2022-08-26 18:13:08 +0530

    Update SOE.cpp

--- Number Theory/SOE.cpp
+++ Number Theory/SOE.cpp
@@ -10,8 +10,8 @@ void sieve_of_eratosthenes(){
   vector<bool> isPrime(n+1, true);
   isPrime[0]=isPrime[1]=false;
 
-  for(int i=2; i<=n; i++){
-    if(isPrime[i] && (long long)i*i<=n){
+  for(int i=2; i*i<=n; i++){
+    if(isPrime[i]){
       for(int j=i*i; j<=n; j+=i){
         isPrime[j]=false;
       }
